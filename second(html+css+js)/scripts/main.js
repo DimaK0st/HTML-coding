d36d0cefad882991c9d1439d3b4b5ad67a6ab186
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-03-27 21:48:00 +0300

    Add Animate

--- second(html+css+js)/scripts/main.js
+++ second(html+css+js)/scripts/main.js
@@ -1,13 +1,51 @@
-window.onscroll = function ()
-{
+window.onscroll = function () {
     let header = document.getElementById('header-web')
 
-    console.log(header)
-    if(window.scrollY>5){
-        header.style.background ='rgba(0,0,0,0.38)'
-    }
-    else {
+    if (window.scrollY > 5) {
+        header.style.background = 'rgba(0,0,0,0.38)'
+    } else {
         header.style.background = 'rgba(0, 0, 0, 0)'
     }
+}
+console.log('hai');
+const animItems = document.querySelectorAll('._anim-items');
+console.log(animItems);
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
 
+    function animOnScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemsHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemsHeight / animStart;
+
+            if (animItemsHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemsHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+
+    setTimeout(() => {
+            animOnScroll()
+        }, 500
+    )
 }
