0069dd30ccd09cff4c8c3e511b5936ef26facde3
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-04-24 22:19:10 +0300

    Add Some JS Functions And Tabs

---
+++ JS/first/js/main.js
@@ -0,0 +1,35 @@
+const btns = document.querySelectorAll('button');
+const wrapper = document.querySelector('.dynamic-btn-block');
+
+btns[0].addEventListener('click', () => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red')
+    } else {
+        btns[1].classList.remove('red')
+    }
+})
+
+btns[1].addEventListener('click', () => {
+    btns[0].classList.toggle('red')
+})
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.tagName === 'BUTTON') {
+        if (event.target.classList.contains('remove-btn')) {
+            const btns = wrapper.querySelectorAll("button")
+            btns[btns.length - 1].remove();
+            return;
+        }
+
+        if (event.target.classList.contains('add-btn')) {
+            const newBtn = document.createElement('button')
+            newBtn.innerText = '0'
+            wrapper.append(newBtn)
+            return;
+        }
+
+        const clickBtn = event.target
+        clickBtn.classList.toggle('red')
+        clickBtn.innerText = ++clickBtn.innerText
+    }
+})
