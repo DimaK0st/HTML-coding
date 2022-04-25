c5baae9c2910164f6de49bbc804590efdc1cea09
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-04-25 23:04:05 +0300

    Add Interval

--- JS/first/js/main.js
+++ JS/first/js/main.js
@@ -1,5 +1,8 @@
 const btns = document.querySelectorAll('button');
-const wrapper = document.querySelector('.dynamic-btn-block');
+const dynamicWrapper = document.querySelector('.dynamic-btn-block');
+const setTimeoutWrapper = document.querySelector('.set-timeout-block');
+const setTimeoutCounter = document.querySelector('.set-timeout-counter');
+let idInterval, direction;
 
 btns[0].addEventListener('click', () => {
     if (!btns[1].classList.contains('red')) {
@@ -13,10 +16,10 @@ btns[1].addEventListener('click', () => {
     btns[0].classList.toggle('red')
 })
 
-wrapper.addEventListener('click', (event) => {
+dynamicWrapper.addEventListener('click', (event) => {
     if (event.target && event.target.tagName === 'BUTTON') {
         if (event.target.classList.contains('red-btn')) {
-            const btns = wrapper.querySelectorAll("button")
+            const btns = dynamicWrapper.querySelectorAll("button")
             btns[btns.length - 1].remove();
             return;
         }
@@ -24,7 +27,7 @@ wrapper.addEventListener('click', (event) => {
         if (event.target.classList.contains('green-btn')) {
             const newBtn = document.createElement('button')
             newBtn.innerText = '0'
-            wrapper.append(newBtn)
+            dynamicWrapper.append(newBtn)
             return;
         }
 
@@ -33,3 +36,46 @@ wrapper.addEventListener('click', (event) => {
         clickBtn.innerText = ++clickBtn.innerText
     }
 })
+
+setTimeoutWrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.tagName === 'BUTTON') {
+        if (event.target.classList.contains('add')) {
+            direction = true
+            clearInterval(idInterval)
+            idInterval = setInterval(counterController, 1000)
+            return;
+        }
+
+        if (event.target.classList.contains('delete')) {
+            direction = false
+            clearInterval(idInterval)
+            idInterval = setInterval(counterController, 1000)
+            return;
+        }
+
+        if (event.target.classList.contains('stop')) {
+            clearInterval(idInterval)
+            return;
+        }
+
+        const clickBtn = event.target
+        clickBtn.classList.toggle('red')
+        clickBtn.innerText = ++clickBtn.innerText
+    }
+})
+
+
+function counterController() {
+    if (direction) {
+        setTimeoutWrapper.append(document.createElement('button'))
+        setTimeoutCounter.innerText = ++setTimeoutCounter.innerText
+    } else {
+        const btns = document.querySelector('.set-timeout-block').querySelectorAll("button")
+        if (btns.length > 3) {
+            setTimeoutCounter.innerText = --setTimeoutCounter.innerText
+            btns[btns.length - 1].remove();
+        } else {
+            clearInterval(idInterval)
+        }
+    }
+}
