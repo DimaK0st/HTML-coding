6bebb54246ec908a58089bf012fa679c64aeeb31
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-05-04 13:09:48 +0300

    Add Calc

--- JS/first/js/es6.js
+++ JS/first/js/es6.js
@@ -14,7 +14,7 @@ User.prototype.allInfo = function () {
     console.log(`Human ${this.human}`)
 }
 
-const ivan = new User('Ivan', 666)
+let ivan = new User('Ivan', 666)
 ivan.hello()
 console.log(ivan)
 ivan.allInfo()
@@ -38,4 +38,101 @@ class UserV2 {
     }
 }
 
+//getter/setter
 
+const persone = {
+    name: "Alex",
+    age: 25,
+
+    get userAge() {
+        return this.age
+    },
+
+    set userAge(num) {
+        this.age = num
+    }
+}
+
+console.log(persone.userAge = 30)
+console.log(persone.userAge)
+
+//Инкапсуляция
+
+function User3(name, age) {
+    this.name = name
+    this.age = age
+
+    this.say = function () {
+        console.log(`Имя пользователя: ${this.name}, возраст ${this.age}`)
+    }
+}
+
+ivan = new User3('Ivan', 10)
+ivan.say()
+
+ivan.name = 'Stepan'
+ivan.age = 30
+
+ivan.say()
+
+
+function User4(name, age) {
+    this.name = name
+    let userAge = age
+
+    this.say = function () {
+        console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`)
+    }
+
+    this.getAge = function () {
+        return userAge;
+    }
+
+    this.setAge = function (age) {
+        if (typeof age === 'number' && age > 0 && age < 150) {
+            userAge = age
+        } else {
+            console.log('Недопустимое значение :(')
+        }
+    }
+
+}
+
+ivan = new User4('Ivan', 10)
+ivan.say()
+
+ivan.name = 'Stepan'
+ivan.setAge(30)
+
+ivan.say()
+
+
+class User5 {
+    constructor(name, age) {
+        this.name = name
+        this._age = age
+    }
+
+    say() {
+        console.log(`Имя пользователя: ${this.name}, возраст ${this._age}`)
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 150) {
+            this._age = age
+        } else {
+            console.log('Недопустимое значение :(')
+        }
+    }
+}
+
+ivan = new User5('Ivan', 10)
+ivan.say()
+
+ivan.age = 30
+
+ivan.say()
