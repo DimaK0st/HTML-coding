67e29ef0e20271d197e4f846534bc6cf529449ac
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-05-17 18:21:39 +0300

    Add Service and Spinner onLoad

---
+++ React/second-app/src/services/MarvelService.js
@@ -0,0 +1,44 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+    _apiKey = 'apikey=d8d91bcae31f6e159f05936ba0ff51d4'
+
+    getResource = async (url) => {
+        const res = await fetch(url)
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`)
+        }
+
+        return await res.json()
+
+
+    }
+
+    getAllCharacters = async () => {
+        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    _transformCharacter = (res) => {
+        res.description = !res.description ? 'No description :)' : res.description
+
+        if (res.description.length > 200) {
+            res.description = res.description.slice(0, 197) + '...'
+        }
+
+        return {
+            name: res.name,
+            description: res.description,
+            thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
+            homepage: res.urls[0].url,
+            wiki: res.urls[1].url,
+        }
+    }
+}
+
+export default MarvelService
