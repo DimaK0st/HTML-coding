7579939c23e2f0a36ca16bda81423d29c843b0a6
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-07-10 23:08:43 +0300

    Improve Main Page

--- React/car-care-app/src/components/pages/main/Main.js
+++ React/car-care-app/src/components/pages/main/Main.js
@@ -1,7 +1,9 @@
 import './Main.scss'
 import image from '../../../assets/mainImg.svg'
+import {useNavigate} from "react-router-dom";
 
 const Main = () => {
+    const navigate = useNavigate();
 
 
     return (
@@ -10,6 +12,8 @@ const Main = () => {
             <h1 className='title'>Car Care</h1>
             <p className='description'> Сайт сделанный для помощи в слежении за своевременной заменой автомобильных
                 запчастей.</p>
+            <br/>
+            <p className='description' style={{cursor:"pointer", color:"orange"}} onClick={()=>navigate('/home')}>Поехали</p>
             <img className={'main-image'} src={image} alt={'Слава Україні'}/>
         </div>
     )
