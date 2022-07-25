f842973e1b70d977d8205fcae3ba4e032b4e0737
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-07-25 22:39:41 +0300

    Add accessories

--- HTML/fourth(html+scss+js+webpack)/src/index.js
+++ HTML/fourth(html+scss+js+webpack)/src/index.js
@@ -3,6 +3,7 @@ import "./styles/reset.scss";
 import "./styles/main.scss";
 import "./styles/header.scss";
 import "./styles/water-suyum.scss";
+import "./styles/accessories.scss";
 import "./styles/footer.scss";
 import "./scripts/main";
 
