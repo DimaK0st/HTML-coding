881a94104ef61ebdee07bc4e9c7ef6ef779840f5
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-07-28 23:56:11 +0300

    Add Image

--- HTML/fourth(html+scss+js+webpack)/src/index.js
+++ HTML/fourth(html+scss+js+webpack)/src/index.js
@@ -5,7 +5,7 @@ import "./styles/header.scss";
 import "./styles/water-suyum.scss";
 import "./styles/accessories.scss";
 import "./styles/healthy-water.scss";
+import "./styles/suyum-blob.scss";
 import "./styles/footer.scss";
 import "./scripts/main";
 
-console.log('hui2')
