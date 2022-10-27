4d306a76d4ad932268ada1fdd695e0bea26b7500
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2022-10-27 21:54:46 +0300

    Improve Scss rendering

--- React/telephone-directory/resources/js/components/headerAndFooter/Header.js
+++ React/telephone-directory/resources/js/components/headerAndFooter/Header.js
@@ -2,6 +2,7 @@ import phoneIcon from '/assets/phone.svg'
 import {useLocation, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 import SearchInput from "../searchInput/SearchInput";
+import './header.scss'
 
 const Header = () => {
     const [phone, setPhone] = useState()
@@ -13,7 +14,9 @@ const Header = () => {
 
     return (
         <header className={phone ? 'header main-header' : 'header'}>
-            <img className='logo' alt={'Слава Україні'} src={phoneIcon} onClick={()=>{navigate('/')}}/>
+            <img className='logo' alt={'Слава Україні'} src={phoneIcon} onClick={() => {
+                navigate('/')
+            }}/>
             {phone ? <SearchInput/> : <div></div>}
             <span className={'todo'}>TODO...</span>
         </header>
