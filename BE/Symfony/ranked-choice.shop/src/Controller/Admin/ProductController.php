c13db4f833134d62bf1a574fb6fbf715a8a78b01
DimaK0st <70113281+Dimak0st@users.noreply.github.com>
2023-08-03 23:54:55 +0300

    Fix OS and Add Product List

---
+++ BE/Symfony/ranked-choice.shop/src/Controller/Admin/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/product", name="admin_product_")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findBy(['isDeleted' => false], ['id' => 'DESC'], 50);
+
+        return $this->render('admin/product/list.html.twig', ['products' => $product]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @Route("/add", name="add")
+     */
+    public function edit(Request $request, int $id): Response
+    {
+        //
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Request $request, int $id): Response
+    {
+        //
+    }
+}
