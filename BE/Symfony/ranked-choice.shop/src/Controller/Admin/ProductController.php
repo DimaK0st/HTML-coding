addd7e4b832adec2f89eecad7fdc5691c8e90807
DimaK0st <70113281+Dimak0st@users.noreply.github.com>
2023-08-04 23:48:46 +0300

    Add edit product

--- BE/Symfony/ranked-choice.shop/src/Controller/Admin/ProductController.php
+++ BE/Symfony/ranked-choice.shop/src/Controller/Admin/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Product;
+use App\Form\EditProductFormType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,8 +29,29 @@ class ProductController extends AbstractController
      * @Route("/edit/{id}", name="edit")
      * @Route("/add", name="add")
      */
-    public function edit(Request $request, int $id): Response
+    public function edit(Request $request, Product $product = null): Response
     {
+        $form = $this->createForm(EditProductFormType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Product saved');
+
+            return $this->redirectToRoute('admin_product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('admin/product/edit.html.twig', [
+            'form' => $form->createView(),
+            'product' => $product,
+        ]);
+
+        dd($product);
         //
     }
 
--- BE/Symfony/ranked-choice.shop/src/Form/EditProductFormType.php
+++ BE/Symfony/ranked-choice.shop/src/Form/EditProductFormType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,6 +23,7 @@ class EditProductFormType extends AbstractType
                 'label' => 'Product title',
                 'attr' => [
                     'placeholder' => 'Enter product title',
+                    'class' => 'form-control',
                 ],
                 'constraints' => [
                     new NotBlank([], 'Please enter product title'),
@@ -31,14 +35,47 @@ class EditProductFormType extends AbstractType
                 'scale' => 2,
                 'html5' => true,
                 'attr' => [
+                    'class' => 'form-control',
                     'placeholder' => 'Enter product price',
+                    'min' => '0',
                     'step' => '0.03',
                 ],
             ])
-            ->add('quantity')
-            ->add('description')
-            ->add('isPublished')
-            ->add('isDeleted')
+            ->add('quantity', IntegerType::class,[
+                'label' => 'Product quantity',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter product quantity',
+                    'min' => '0',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Product description',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter product description',
+                ],
+            ])
+            ->add('isPublished', CheckboxType::class, [
+                'label' => 'Publish product?',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label',
+                ],
+            ])
+            ->add('isDeleted', CheckboxType::class, [
+                'label' => 'Delete product?',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label',
+                ],
+            ])
         ;
     }
 
