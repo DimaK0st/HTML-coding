b5b66ba4e86d7bd06f4db4daa017f56a78e73528
DimaK0st <70113281+Dimak0st@users.noreply.github.com>
2023-08-28 23:34:53 +0300

    Add Save Cart

--- BE/Symfony/ranked-choice.shop/src/Controller/Main/CartApiController.php
+++ BE/Symfony/ranked-choice.shop/src/Controller/Main/CartApiController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller\Main;
 
+use App\Entity\Cart;
+use App\Entity\CartProduct;
+use App\Repository\CartProductRepository;
+use App\Repository\CartRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,10 +21,37 @@ class CartApiController extends AbstractController
     /**
      * @Route("/cart", methods="POST", name="cart_save")
      */
-    public function saveCart(Request $request): Response
+    public function saveCart(Request $request, CartRepository $cartRepository, CartProductRepository $cartProductRepository, ProductRepository $productRepository): Response
     {
         $sessionId = $request->cookies->get('PHPSESSID');
         $productId = $request->request->get('productId');
+
+        $product = $productRepository->findOneBy(['uuid'=>$productId]);
+
+        $cart = $cartRepository->findOneBy(['sessionId'=>$sessionId]);
+
+        if (!$cart) {
+            $cart = new Cart();
+            $cart->setSessionId($sessionId);
+        }
+
+        $cartProduct = $cartProductRepository->findOneBy(['cart'=>$cart, 'product'=>$product]);
+
+        if ($cartProduct) {
+            $cartProduct->setQuantity($cartProduct->getQuantity() + 1);
+        } else {
+            $cartProduct = new CartProduct();
+            $cartProduct->setCart($cart);
+            $cartProduct->setProduct($product);
+            $cartProduct->setQuantity(1);
+        }
+
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($cart);
+        $entityManager->persist($cartProduct);
+        $entityManager->flush();
+
 
         return new JsonResponse([
             'success' => true,
