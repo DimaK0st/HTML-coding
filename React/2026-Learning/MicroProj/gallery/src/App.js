76c54f4171093ca211cca1831673147667a9af16
Dima Kostetskyi <wwwmistik13@gmail.com>
2026-01-14 23:52:25 +0200

    Add gallery

---
+++ React/2026-Learning/MicroProj/gallery/src/App.js
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+import './index.scss';
+import {Collection} from "./Collection";
+
+const categories = [
+    {"name": "Все"},
+    {"name": "Море"},
+    {"name": "Горы"},
+    {"name": "Архитектура"},
+    {"name": "Города"}
+];
+
+function App() {
+    const [searchValue, setSearchValue] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [categoryId, setCategoryId] = useState(0);
+    const [page, setPage] = useState(1);
+    const [collections, setCollections] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(`https://626c2fbd50a310b8a341637f.mockapi.io/test/ajax/PhotoCollection?${categoryId ? 'category=' + categoryId : ''}`)
+            .then(res => res.json())
+            .then(json => {
+                setCollections(json);
+                console.log(json);
+            })
+            .catch(err => {
+                console.warn(err);
+                alert('Ошибка при получении данных');
+            })
+            .finally(() => setIsLoading(false));
+    }, [categoryId])
+
+    return (
+        <div className="App">
+            <h1>Моя коллекция фотографий</h1>
+            <div className="top">
+                <ul className="tags">
+                    {categories?.map((category, index) => (
+                        <li
+                            className={categoryId === index ? 'active' : ''}
+                            key={index}
+                            onClick={() => setCategoryId(index)}
+                        >{category.name}</li>
+                    ))}
+                </ul>
+                <input
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    className="search-input" placeholder="Поиск по названию"/>
+            </div>
+            <div className="content">
+                {isLoading ?
+                    <h2>Is loading ....</h2> :
+                    collections
+                        ?.filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase())).map((obj, index) => (
+                        // console.log(obj);
+                        <Collection key={index} name={obj.name} images={obj.photos}/>
+                    ))
+                }
+            </div>
+            <ul className="pagination">
+                <li>1</li>
+                <li className="active">2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
