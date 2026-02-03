1616bf582203cd128f9bf4cf30fb90e606c13f7f
DimaKost <wwwmistik13@gmail.com>
2026-02-03 02:43:31 +0200

    Add GroupVariants and ProductImage components; update layout and product page structure

--- React/2026-Learning/next-pizza/components/shared/searchInput.tsx
+++ React/2026-Learning/next-pizza/components/shared/searchInput.tsx
@@ -60,7 +60,7 @@ export const SearchInput: React.FC<Props> = ({className}) => {
                             onClick={onClickItem}
                             key={product.id}
                             className="flex items-center gap-3 px-3 py-2 hover:bg-primary/10 cursor-pointer"
-                            href={"/product/" + product.id}
+                            href={"/app/(root)/product/" + product.id}
                         >
                             <img className="rounded-sm h-8 w-8"
                                  src={product.imageUrl || '/placeholder.png'}
